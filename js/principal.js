e6a4f8ba774cfa7c872d873437fea9c009f51de1
Gizele Santos <gizele.santos@ibm.com>
2019-10-11 11:25:35 -0300

    Introdução ao JavaScript - Aula 1 (alert, console.log, document, querySelector, textContent e boas práticas)

---
+++ js/principal.js
@@ -0,0 +1,15 @@
+	//alert("Ola mundo");
+	//console.log("Ola mundo");
+	//console.log(document);
+		
+	//console.log(document.querySelector("h1")); //seleciona e imprime
+
+	//var titulo = document.querySelector("h1");
+	//console.log(titulo);
+	//console.log(titulo.textContent);
+
+	//buscnado pela class no h1
+
+	var titulo = document.querySelector(".titulo"); //seletor para classe é .
+
+	titulo.textContent = "Aparecida Nutricionista";
